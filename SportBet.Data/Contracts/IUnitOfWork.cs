08d697c4c6dbde5b0601bfa3ae8cc7c9add84288
Nickose777 <my_personal@mail.ua>
2017-03-21 23:07:03 +0200

    Added SportBet.Data.Contracts.Repositories and IUnitOfWork

---
+++ SportBet.Data/Contracts/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportBet.Data.Contracts.Repositories;
+
+namespace SportBet.Data.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IBetRepository Bets { get; }
+        IBookmakerRepository Bookmakers { get; }
+        IClientRepository Clients { get; }
+        ICoefficientRepository Coefficients { get; }
+        ICountryRepository Countries { get; }
+        IEventRepository Events { get; }
+        IParticipantRepository Participants { get; }
+        IParticipationRepository Participations { get; }
+        ISportRepository Sports { get; }
+        ITournamentRepository Tournaments { get; }
+
+        void Commit();
+    }
+}
---
+++ SportBet.Data/Contracts/Repositories/IParticipantRepository.cs
@@ -0,0 +1,13 @@
+﻿using SportBet.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportBet.Data.Contracts.Repositories
+{
+    public interface IParticipantRepository : IRepository<ParticipantEntity>
+    {
+    }
+}
