325b0f3bb12b2d9402152d406b63e8a9b73c3275
Nickose777 <my_personal@mail.ua>
2017-03-22 03:38:23 +0200

    Created IAuthService and some DTO objects

---
+++ SportBet.Controllers/Contracts/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using SportBet.Controllers.DTOModels;
+using SportBet.Controllers.ResultTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportBet.Controllers.Contracts
+{
+    public interface IAuthService
+    {
+        AuthResult Register(ClientRegisterDTO clientRegisterDTO);
+        AuthResult LoginAsSuperUser(UserLoginDTO adminLoginDTO);
+        AuthResult LoginAsAdmin(UserLoginDTO adminLoginDTO);
+        AuthResult LoginAsAnalyst(UserLoginDTO adminLoginDTO);
+        AuthResult LoginAsBookmaker(UserLoginDTO clientLoginDTO);
+        AuthResult LoginAsClient(UserLoginDTO clientLoginDTO);
+    }
+}
---
+++ SportBet.Controllers/ResultTypes/AuthResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportBet.Controllers.ResultTypes
+{
+    public class AuthResult
+    {
+        public string Message { get; private set; }
+        public bool IsSuccessful { get; private set; }
+
+        public AuthResult(string message, bool success)
+        {
+            this.Message = message;
+            this.IsSuccessful = success;
+        }
+    }
+}
