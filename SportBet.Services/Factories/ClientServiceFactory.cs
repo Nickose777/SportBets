de5d20af4c9a40f6078a80350f9ca519041be7b6
Nickose777 <my_personal@mail.ua>
2017-03-25 14:40:32 +0200

    Created ClientServiceFactory for User Login

---
+++ SportBet.Services/Factories/ClientServiceFactory.cs
@@ -0,0 +1,13 @@
+﻿using SportBet.Services.Contracts.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportBet.Services.Factories
+{
+    class ClientServiceFactory : IServiceFactory
+    {
+    }
+}
