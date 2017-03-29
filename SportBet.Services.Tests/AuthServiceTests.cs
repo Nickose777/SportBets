178e320b8827a0157301701083319492d099a905
Nickose777 <my_personal@mail.ua>
2017-03-29 23:49:23 +0300

    AuthService tested EstablishConnection method

--- SportBet.Services.Tests/AuthServiceTests.cs
+++ SportBet.Services.Tests/AuthServiceTests.cs
@@ -1,22 +1,83 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SportBet.Data.Contracts;
 using SportBet.Data.Contracts.Repositories;
 using SportBet.Services.Contracts.Services;
 using SportBet.Services.Providers;
+using SportBet.Core.Entities;
+using System.Linq.Expressions;
+using System.Collections.Generic;
 
 namespace SportBet.Services.Tests
 {
     [TestClass]
     public class AuthServiceTests
     {
+        private Mock<IUnitOfWork> unitOfWork;
+        private IAuthService service;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            unitOfWork = new Mock<IUnitOfWork>();
+            service = new AuthService(unitOfWork.Object);
+        }
+
+        [TestMethod]
+        public void EstablishConnectionWithoutAdminCreateDefaultSuperuser()
+        {
+            List<UserEntity> emptyList = new List<UserEntity>();
+            unitOfWork.Setup(u => u.Users.GetAll(It.IsAny<Expression<Func<UserEntity, bool>>>()))
+                .Returns(emptyList);
+            unitOfWork.Setup(u => u.Accounts.CreateDefaultSuperuser(It.IsAny<string>()));
+
+            service.EstablishConnection();
+
+            unitOfWork.Verify(u => u.Accounts.CreateDefaultSuperuser(It.IsAny<string>()), Times.Once());
+        }
+        [TestMethod]
+        public void EstablishConnectionWithAdminNoCreateDefaultSuperuser()
+        {
+            List<UserEntity> listWithOneUser = new List<UserEntity>() { new UserEntity() };
+            unitOfWork.Setup(u => u.Users.GetAll(It.IsAny<Expression<Func<UserEntity, bool>>>()))
+                .Returns(listWithOneUser);
+            unitOfWork.Setup(u => u.Accounts.CreateDefaultSuperuser(It.IsAny<string>()));
+
+            service.EstablishConnection();
+
+            unitOfWork.Verify(u => u.Accounts.CreateDefaultSuperuser(It.IsAny<string>()), Times.Never());
+        }
+        [TestMethod]
+        public void EstablishConnectionWithoutAdminCreateDefaultSuperuserReturnsTrue()
+        {
+            List<UserEntity> emptyList = new List<UserEntity>();
+            unitOfWork.Setup(u => u.Users.GetAll(It.IsAny<Expression<Func<UserEntity, bool>>>()))
+                .Returns(emptyList);
+            unitOfWork.Setup(u => u.Accounts.CreateDefaultSuperuser(It.IsAny<string>()));
+
+            bool established = service.EstablishConnection();
+
+            Assert.IsTrue(established);
+        }
+        [TestMethod]
+        public void EstablishConnectionUnitOfWorkThrowsExceptionReturnsFalse()
+        {
+            List<UserEntity> emptyList = new List<UserEntity>();
+            unitOfWork.Setup(u => u.Users.GetAll(It.IsAny<Expression<Func<UserEntity, bool>>>()))
+                .Returns(emptyList);
+            unitOfWork.Setup(u => u.Accounts.CreateDefaultSuperuser(It.IsAny<string>())).Throws(new Exception());
+
+            bool established = service.EstablishConnection();
+
+            Assert.IsFalse(established);
+        }
+
         [TestMethod]
         public void DisposesUnitOfWork()
         {
-            Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
             unitOfWork.Setup(u => u.Dispose()).Verifiable();
-            IAuthService service = new AuthService(unitOfWork.Object);
 
             service.Dispose();
             unitOfWork.Verify();
