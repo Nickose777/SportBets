77b6e4e98ce4a574550b303190bd0d9ad5cf5db0
Nickose777 <my_personal@mail.ua>
2017-04-11 18:35:36 +0300

    Bugs. Validation on client edit

--- SportBet.Services/Providers/ClientServices/ClientClientService.cs
+++ SportBet.Services/Providers/ClientServices/ClientClientService.cs
@@ -26,23 +26,30 @@ namespace SportBet.Services.Providers.ClientServices
 
             if (login == Session.CurrentUserLogin)
             {
-                try
+                if (Validate(clientEditDTO, ref message))
                 {
-                    int id = unitOfWork.Users.GetIdByLogin(login);
-                    ClientEntity clientEntity = unitOfWork.Clients.Get(id);
+                    try
+                    {
+                        int id = unitOfWork.Users.GetIdByLogin(login);
+                        ClientEntity clientEntity = unitOfWork.Clients.Get(id);
 
-                    clientEntity.FirstName = clientEditDTO.FirstName;
-                    clientEntity.LastName = clientEditDTO.LastName;
-                    clientEntity.PhoneNumber = clientEditDTO.PhoneNumber;
-                    clientEntity.DateOfBirth = clientEditDTO.DateOfBirth;
+                        clientEntity.FirstName = clientEditDTO.FirstName;
+                        clientEntity.LastName = clientEditDTO.LastName;
+                        clientEntity.PhoneNumber = clientEditDTO.PhoneNumber;
+                        clientEntity.DateOfBirth = clientEditDTO.DateOfBirth;
 
-                    unitOfWork.Commit();
+                        unitOfWork.Commit();
 
-                    message = "Successfully changed client's information";
+                        message = "Successfully changed client's information";
+                    }
+                    catch (Exception ex)
+                    {
+                        message = ExceptionMessageBuilder.BuildMessage(ex);
+                        success = false;
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    message = ExceptionMessageBuilder.BuildMessage(ex);
                     success = false;
                 }
             }
@@ -53,6 +60,39 @@ namespace SportBet.Services.Providers.ClientServices
             }
 
             return new ServiceMessage(message, success);
+        }
+
+        private bool Validate(ClientEditDTO clientEditDTO, ref string message)
+        {
+            bool isValid = true;
+
+            if (String.IsNullOrEmpty(clientEditDTO.LastName))
+            {
+                message = "Last name must not be empty";
+                isValid = false;
+            }
+            else if (String.IsNullOrEmpty(clientEditDTO.FirstName))
+            {
+                message = "First name must not be empty";
+                isValid = false;
+            }
+            else if (String.IsNullOrEmpty(clientEditDTO.PhoneNumber))
+            {
+                message = "Phone number name must not be empty";
+                isValid = false;
+            }
+            else if (clientEditDTO.DateOfBirth.Year < DateTime.Now.Year - 100)
+            {
+                message = "You are too old. Sorry";
+                isValid = false;
+            }
+            else if (clientEditDTO.DateOfBirth.Year > DateTime.Now.Year - 18)
+            {
+                message = "You are too young. Sorry";
+                isValid = false;
+            }
+
+            return isValid;
         }
 
         public ServiceMessage Delete(string login)
