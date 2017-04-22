0d16d41844c90cc0a810c8fe56e7ddcd304b94ee
Nickose777 <my_personal@mail.ua>
2017-04-23 00:36:48 +0300

    As an admin I want to add coefficients. Included CoefficientCreateDTO

---
+++ SportBet.Services/DTOModels/Create/CoefficientCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿using SportBet.Services.DTOModels.Base;
+
+namespace SportBet.Services.DTOModels.Create
+{
+    public class CoefficientCreateDTO : CoefficientBaseDTO
+    {
+        public string Description { get; set; }
+    }
+}
